7cad00df6d756ce3d1486c7aa7fa326250944f99
nadavschwartz <nadavschwartz58@gmail.com>
2021-03-11 14:14:35 -0800

    Homescreen && product and react-bootstrap setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,22 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import "./bootstrap.min.css";
+import "./index.css";
+import HomeScreen from "./screens/HomeScreen";
 const App = () => {
-  return <>hey</>;
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
