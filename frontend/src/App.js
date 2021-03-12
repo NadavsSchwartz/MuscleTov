729eefb3914fb7615f45e32c91af1ade5cd9dabc
nadavschwartz <nadavschwartz58@gmail.com>
2021-03-11 17:53:55 -0800

    init express backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,17 +5,20 @@ import Header from "./components/Header";
 import "./bootstrap.min.css";
 import "./index.css";
 import HomeScreen from "./screens/HomeScreen";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import ProductScreen from "./screens/ProductScreen";
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Route exact path="/" component={HomeScreen} />
+          <Route path="/product/:id" component={ProductScreen} />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 };
 
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -8,22 +8,29 @@ import {
   Navbar,
   NavDropdown,
 } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
 const Header = () => {
   return (
     <header>
       <Navbar bg="light" expand="lg" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="#home">MuscleTov</Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand>MuscleTov</Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
-              <Nav.Link href="#home">
-                <i className="fas fa-shopping-cart"></i>Cart
-              </Nav.Link>
-              <Nav.Link href="#link">
-                <i className="fas fa-user"></i>Sign In
-              </Nav.Link>
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <i className="fas fa-shopping-cart"></i>Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <Nav.Link>
+                  <i className="fas fa-user"></i>Sign In
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
