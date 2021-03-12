4fb12fc415574727c3ce4ef338c6b13cb2d5a7f8
nadavschwartz <nadavschwartz58@gmail.com>
2021-03-12 14:11:36 -0800

    login page design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,15 +8,17 @@ import HomeScreen from "./screens/HomeScreen";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
+import LoginScreen from "./screens/LoginScreen";
 const App = () => {
   return (
     <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <Route exact path="/" component={HomeScreen} />
+          <Route path="/login" component={LoginScreen} />
           <Route path="/product/:id" component={ProductScreen} />
           <Route path="/cart/:id?" component={CartScreen} />
+          <Route exact path="/" component={HomeScreen} />
         </Container>
       </main>
       <Footer />
