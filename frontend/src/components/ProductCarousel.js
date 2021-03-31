54791ae55c0a337333c4771d71e2d399b189ce4f
nadavschwartz <nadavschwartz58@gmail.com>
2021-03-31 12:26:23 -0700

    carousel image update && header search button

--- frontend/src/components/ProductCarousel.js
+++ frontend/src/components/ProductCarousel.js
@@ -25,7 +25,11 @@ const ProductCarousel = () => {
       {products.map((product) => (
         <Carousel.Item key={product._id}>
           <Link to={`/product/${product._id}`}>
-            <Image src={product.image} alt={product.name} fluid />
+            <Image
+              src={product.image}
+              alt={product.name}
+              className="d-block w-100"
+            />
             <Carousel.Caption className="carousel-caption">
               <h2>
                 {product.name} (${product.price})
