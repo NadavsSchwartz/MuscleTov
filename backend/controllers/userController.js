fa5e7fc2094736e25ca87223a370c806e62380eb
nadavschwartz <nadavschwartz58@gmail.com>
2021-04-01 08:12:43 -0700

    better email validation

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -29,12 +29,19 @@ const authUser = asyncHandler(async (req, res) => {
 // @access  Public
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
-
   const userExists = await User.findOne({ email });
 
   if (userExists) {
     res.status(400);
     throw new Error("User already exists");
+  }
+
+  var pattern = new RegExp(
+    /^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i
+  );
+  if (!pattern.test(email)) {
+    res.status(400);
+    throw new Error("Please enter a valid email address.");
   }
 
   const user = await User.create({
